b8107667046be717923cbefea767f737cbf2e813
vijay-prajapati-occams <vijay.prajapati@occamsadvisory.com>
2026-08-31 21:14:30 +0530

    error fixed

---
+++ competitive-intelligence/backend/eslint.config.js
@@ -0,0 +1,21 @@
+const tseslint = require("typescript-eslint");
+
+module.exports = tseslint.config(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": "off",
+    },
+  },
+);
